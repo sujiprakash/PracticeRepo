44c48deef1779621db7537e40af38ac669651b5b
sujiprakash <prakash28491@gmail.com>
2021-03-30 15:45:41 +0530

    Changes in AppTest.java

--- src/test/java/org/sample/FrameworkJunit/AppTest.java
+++ src/test/java/org/sample/FrameworkJunit/AppTest.java
@@ -16,13 +16,16 @@ public class AppTest{
 	@BeforeClass
 	public static void ClassStart() {
 		System.out.println("Launching browser");
-		System.out.println("class start");
+		System.out.println("class started");
+		System.out.println("class started");
+		System.out.println("class started");
+
 	}
 	
 	@AfterClass
 	public static void ClassEnd() {
 		System.out.println("Closing browser");
-		System.out.println("class end");
+		System.out.println("class ended");
 	}
 	
 	@Before
